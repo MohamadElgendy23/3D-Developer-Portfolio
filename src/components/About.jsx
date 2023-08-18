1b1f2dd8459601eae41d944e90de67c999f6ac2d
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-17 20:56:02 -0600

    alittle styling edits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { styles } from "../styles";
 import { services } from "../myinformaton";
 import { Tilt } from "react-tilt";
@@ -9,16 +8,17 @@ const About = () => {
   return (
     <div className="bg-hero-pattern">
       <motion.div>
-        <p className={styles.sectionSubText}>About Me</p>
+        <p className={`${styles.sectionSubText} text-violet-500`}>About Me</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 0.1)}></motion.p>Hello! My name is
-      Mohamad Elgendy and I am a Full Stack Developer. I am highly passionate
-      about anything related to programming and improving my skills. I am
-      experienced in multiple languages and frameworks such as JavaScript,
-      TypeScript, Java, C#, React.js, Node.js, and Three.js. I truly believe
-      that with my skills and personality, I can mesh well with any team to
-      build software that will last a lifetime!
+      <motion.p variants={fadeIn("", "", 0.1, 0.1)}></motion.p>Hello! My name is{" "}
+      <span className="text-violet-500">Mohamad Elgendy</span> and I am a Full
+      Stack Developer. I am highly passionate about anything related to
+      programming and improving my skills. I am experienced in multiple popular
+      languages and frameworks such as JavaScript, TypeScript, Java, C#,
+      React.js, Node.js, and Three.js. I truly believe that with my skills and
+      personality, I can mesh well with any team to build software that will
+      last a lifetime!
     </div>
   );
 };

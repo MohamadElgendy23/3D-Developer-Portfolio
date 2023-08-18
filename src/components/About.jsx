2da0f087e62b457e4049831120acac31166267d2
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-17 21:12:42 -0600

    adding info cards in about page to represent information about me.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 import { styles } from "../styles";
-import { services } from "../myinformaton";
+import { services as myInfo } from "../myinformaton";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
+
+const InformationCard = () => {};
 
 const About = () => {
   return (
@@ -11,14 +13,22 @@ const About = () => {
         <p className={`${styles.sectionSubText} text-violet-500`}>About Me</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 0.1)}></motion.p>Hello! My name is{" "}
-      <span className="text-violet-500">Mohamad Elgendy</span> and I am a Full
-      Stack Developer. I am highly passionate about anything related to
-      programming and improving my skills. I am experienced in multiple popular
-      languages and frameworks such as JavaScript, TypeScript, Java, C#,
-      React.js, Node.js, and Three.js. I truly believe that with my skills and
-      personality, I can mesh well with any team to build software that will
-      last a lifetime!
+      <motion.p variants={fadeIn("", "", 0.1, 0.1)}>
+        Hello! My name is{" "}
+        <span className="text-violet-500">Mohamad Elgendy</span> and I am a Full
+        Stack Developer. I am highly passionate about anything related to
+        programming and improving my skills. I am experienced in multiple
+        popular languages and frameworks such as JavaScript, TypeScript, Java,
+        C#, React.js, Node.js, and Three.js. I truly believe that with my skills
+        and personality, I can mesh well with any team to build software that
+        will last a lifetime!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {myInfo.map((info, index) => {
+          <InformationCard key={index}>{info}</InformationCard>;
+        })}
+      </div>
     </div>
   );
 };
